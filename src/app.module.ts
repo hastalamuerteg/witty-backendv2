ca19f45069fc7c38351619f186ed34844d78f703
Gabriel Vicente <gabriel.vicent@hotmail.com>
2023-02-09 21:44:05 -0300

    testing nest resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FlashcardsController } from './flashcards.controller';
+import { FlashcardsModule } from './flashcards/flashcards.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, FlashcardsController],
+  imports: [FlashcardsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
